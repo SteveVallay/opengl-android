84f88ee94ad5fcef68573423feaa90f73fab7b80
zhibinw <zhibinwang.q@gmail.com>
2013-03-31 16:55:22 +0800

    draw a triangle using opengl.

--- src/net/obviam/opengl/GlRenderer.java
+++ src/net/obviam/opengl/GlRenderer.java
@@ -6,7 +6,7 @@ package net.obviam.opengl;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-import android.content.Context;
+import android.opengl.GLU;
 import android.opengl.GLSurfaceView.Renderer;
 
 /**
@@ -15,29 +15,49 @@ import android.opengl.GLSurfaceView.Renderer;
  */
 public class GlRenderer implements Renderer {
 
-//	private Context context;	// the application context
-
+	private Triangle 	triangle;	// the triangle to be drawn
+	
 	/** Constructor to set the handed over context */
-//	public GlRenderer(Context context) {
-//		this.context = context;
-//	}
+	public GlRenderer() {
+		this.triangle = new Triangle();
+	}
 
 	@Override
 	public void onDrawFrame(GL10 gl) {
-		// TODO Auto-generated method stubs
+		// clear Screen and Depth Buffer
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+
+		// Reset the Modelview Matrix
+		gl.glLoadIdentity();
+
+		// Drawing
+		gl.glTranslatef(0.0f, 0.0f, -5.0f);		// move 5 units INTO the screen
+												// is the same as moving the camera 5 units away
+//		gl.glScalef(0.5f, 0.5f, 0.5f);			// scale the triangle to 50% 
+												// otherwise it will be too large
+		triangle.draw(gl);						// Draw the triangle
 
 	}
 
 	@Override
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
-		// TODO Auto-generated method stub
+		if(height == 0) { 						//Prevent A Divide By Zero By
+			height = 1; 						//Making Height Equal One
+		}
 
+		gl.glViewport(0, 0, width, height); 	//Reset The Current Viewport
+		gl.glMatrixMode(GL10.GL_PROJECTION); 	//Select The Projection Matrix
+		gl.glLoadIdentity(); 					//Reset The Projection Matrix
+
+		//Calculate The Aspect Ratio Of The Window
+		GLU.gluPerspective(gl, 45.0f, (float)width / (float)height, 0.1f, 100.0f);
+
+		gl.glMatrixMode(GL10.GL_MODELVIEW); 	//Select The Modelview Matrix
+		gl.glLoadIdentity(); 					//Reset The Modelview Matrix
 	}
 
 	@Override
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
