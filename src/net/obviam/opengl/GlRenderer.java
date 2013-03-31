2fe76ed3eb5346fe0b7c7f82e6ff0586fa3e78e3
zhibinw <zhibinwang.q@gmail.com>
2013-03-31 16:34:03 +0800

    sample for use GLSurfaceView.

---
+++ src/net/obviam/opengl/GlRenderer.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package net.obviam.opengl;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView.Renderer;
+
+/**
+ * @author impaler
+ *
+ */
+public class GlRenderer implements Renderer {
+
+//	private Context context;	// the application context
+
+	/** Constructor to set the handed over context */
+//	public GlRenderer(Context context) {
+//		this.context = context;
+//	}
+
+	@Override
+	public void onDrawFrame(GL10 gl) {
+		// TODO Auto-generated method stubs
+
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
